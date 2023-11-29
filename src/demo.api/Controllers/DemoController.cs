ef2d2d44be66c7db88c471ff9cc2ec35b1551fa7
Chaiminda Chandrasekara <chaminda.chandrasekara@ifs.com>
2023-11-29 16:06:06 +0100

    app config with kv reading example

--- src/demo.api/Controllers/DemoController.cs
+++ src/demo.api/Controllers/DemoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace demo.api.Controllers;
 
@@ -6,21 +7,42 @@ namespace demo.api.Controllers;
 [Route("api")]
 public class DemoController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
+    
     private readonly ILogger<DemoController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public DemoController(ILogger<DemoController> logger)
+    public DemoController(ILogger<DemoController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     [HttpGet("Test")]
     public string Test()
     {
         return "API is working...";
+    }
+
+    [HttpGet("ListAppConfigs")]
+    public string ListAppConfigs()
+    {
+        StringBuilder sb = new ();
+        sb.AppendLine("------------------------------------------------------");
+        sb.AppendLine("App Config Values");
+        sb.AppendLine("------------------------------------------------------");
+        sb.AppendLine($"DemoSharedConfig1: {_configuration["DemoSharedConfig1"]}");
+        sb.AppendLine($"DemoSharedConfig2: {_configuration["DemoSharedConfig2"]}");
+        sb.AppendLine($"DemoConfig1: {_configuration["DemoConfig1"]}");
+        sb.AppendLine($"DemoConfig2: {_configuration["DemoConfig2"]}");
+        sb.AppendLine("------------------------------------------------------");
+        sb.AppendLine("App Config KV Secret Values");
+        sb.AppendLine("------------------------------------------------------");
+        sb.AppendLine($"DemoSharedSecret1: {_configuration["DemoSharedSecret1"]}");
+        sb.AppendLine($"DemoSharedSecret2: {_configuration["DemoSharedSecret2"]}");
+        sb.AppendLine($"DemoBGSecret1: {_configuration["DemoBGSecret1"]}");
+        sb.AppendLine($"DemoBGSecret2: {_configuration["DemoBGSecret2"]}");
+        sb.AppendLine("------------------------------------------------------");
+
+        return sb.ToString();
     }
 }
--- src/demo.api/Program.cs
+++ src/demo.api/Program.cs
@@ -1,4 +1,32 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    DefaultAzureCredential azureCredentials = new(
+        // Provide tenant id as shwon below or specify environment variable AZURE_TENANT_ID
+        //new DefaultAzureCredentialOptions { 
+        //    TenantId = "tenat-id-here"
+        //}
+        );
+    options.Connect(
+        new Uri("https://ch-wi-dev-euw-001-appconfig-ac.azconfig.io"),
+        azureCredentials);
+
+    options
+            .Select(KeyFilter.Any, "ch-wi-dev-euw-001-rg")
+            .Select(KeyFilter.Any, "ch-wi-dev-euw-001-rg-blue");
+
+    SecretClient secretClient = new(
+        new Uri("https://ch-wi-dev-euw-001-kv.vault.azure.net/"),
+        azureCredentials);
+
+    options.ConfigureKeyVault(kv =>
+        kv.Register(secretClient));
+   });
 
 // Add services to the container.
 
